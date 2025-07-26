09a5027b515a1bc552cd9b67fc52f40b2be61f27
moose96 <piotr.losiak96@gmail.com>
2025-07-26 14:01:04 +0200

    chore: cleanup package.json and update tools settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,13 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  { ignores: ['**/dist', 'src/tests'] },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+);
